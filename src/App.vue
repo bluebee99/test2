816d8787c60628f7c2b82ec90859f75e7b9cddda
hiro186 <imoimoto99@gmail.com>
2025-05-31 20:26:05 +0900

    初期コミット

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Welcome to Vue.js + Express!')
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style> 
